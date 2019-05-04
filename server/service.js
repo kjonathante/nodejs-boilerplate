3c971356bc5c096fea6fd50c1cd1898768d5d34a
Kit Jonathan Te <kjonathante@gmail.com>
2019-05-04 11:30:03 -0700

    Created test infrastructure.

---
+++ server/service.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+const service = express();
+
+service.get('/service/:location', (req, res, next) => {
+    res.json({ result: req.params.location });
+});
+
+service.put('/service/:intent/:port', (req, res, next) => {
+    const serviceIntent = req.params.intent;
+    const servicePort = req.params.port;
+
+    const serviceIp = req.connection.remoteAddress.includes('::')
+        ? `[${req.connection.remoteAddress}]` : req.connection.remoteAddress;
+
+    res.json({ result: `${serviceIntent} at ${serviceIp}:${servicePort}` });
+});
+
+module.exports = service;
